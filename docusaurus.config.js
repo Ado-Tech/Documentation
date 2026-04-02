238c944c2bb591adde522c98971d1c3d63c54baf
Natalievalverde <Natalie.Valverde@ado-tech.com>
2026-04-02 18:48:16 -0500

    ajuste baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,16 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ado-tech.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'Ado-Tech',// Usually your GitHub org/user name.
+  projectName: 'Documentation',// Usually your repo name.
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
